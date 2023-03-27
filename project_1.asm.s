d42f2e5449931cf3598050ec8395ff4a064aa4d3
Xaiver71 <malcolmgray10@gmail.com>
2023-03-27 18:42:36 -0400

    Added sort numers and check input function

--- project_1.asm.s
+++ project_1.asm.s
@@ -9,3 +9,17 @@ li $v0, 8
 la $a0, input
 li $a1, 11
 syscall
+
+storeInput:
+move $t3, $a0
+lb $s2, ($t3)
+
+checkInput:
+beq $t0, 10, output
+
+sort_numbers:
+addi $t0, $t0, 1
+bge $s2, 97, lowercaseU
+bge $s2, 65, uppercaseU
+bge $s2, 48, numbers 
+
